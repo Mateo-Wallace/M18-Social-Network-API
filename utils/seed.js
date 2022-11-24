5257db67c59d10c96e8cfa238fe1e6dd7e7b3182
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-24 02:31:13 -0500

    edited comments on seeds

--- utils/seed.js
+++ utils/seed.js
@@ -7,21 +7,18 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
   console.log('connected');
 
-  // Drop existing courses
+  // Drop existing users
   await User.deleteMany({});
 
-  // Drop existing students
+  // Drop existing thoughts
   await Thought.deleteMany({});
 
-  // Add courses to the collection and await the results
+  // Add users from data
   await User.collection.insertMany(users);
-  // USERS REQUIRED AND CREATED IN DATA.JS
 
-  // Add students to the collection and await the results
+  // Add thoughts from data
   await Thought.collection.insertMany(thoughts);
-  // THOUGHTS REQUIRED AND CREATED IN DATA.JS
 
-  // Log out the seed data to indicate what should appear in the database
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
