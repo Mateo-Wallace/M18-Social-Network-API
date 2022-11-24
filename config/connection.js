c576b1c59f44b5aa36018d65bbe4a6dbfe5dc56c
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-24 01:25:54 -0500

    restructured config connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
-// Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
-
-connect(connectionString, {
+// After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the
+// application name and add your Atlas connection string as a Config Var. 
+// Node will look for this environment variable and if it exists, it will use it. 
+// Otherwise, it will assume that you are running this application locally
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialMediaDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
 
-module.exports = connection;
+module.exports = mongoose.connection;
