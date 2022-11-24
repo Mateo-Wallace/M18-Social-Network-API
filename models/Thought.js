db2d4214dcaa26ec06b2a948e91f9697f50b0a8a
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-23 21:44:46 -0500

    first draft Thought model complete

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,24 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+// const dateFormat = require('../utils/dateFormat')
 
-const assignmentSchema = new Schema(
+const thoughtSchema = new Schema(
   {
-    assignmentId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    assignmentName: {
+    thoughtText: {
       type: String,
       required: true,
       maxlength: 50,
-      minlength: 4,
-      default: 'Unnamed assignment',
-    },
-    score: {
-      type: Number,
-      required: true,
-      default: () => Math.floor(Math.random() * (100 - 70 + 1) + 70),
     },
     createdAt: {
       type: Date,
       default: Date.now,
     },
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    reactions: [reactionSchema], //reference activity 17 and 18
   },
   {
     toJSON: {
@@ -31,4 +28,11 @@ const assignmentSchema = new Schema(
   }
 );
 
-module.exports = assignmentSchema;
+// virtual for reaction count
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema)
+
+module.exports = Thought;
