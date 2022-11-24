10c14ec3d5767e9b52cc7efa7923701d780e00a4
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-23 21:19:59 -0500

    pseudocode for userController

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,15 +10,22 @@ const {
 } = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 // /api/users/:userId/friends
-router.route('/:userId/friends').post(addFriend);
+router.route('/:userId/friends')
+  .post(addFriend);
 
 // /api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').delete(removeFriend);
+router.route('/:userId/friends/:friendId')
+  .delete(removeFriend);
 
 module.exports = router;
