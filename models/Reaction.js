d8ef23c0a9bf0066fd639140b45e32a848eddacd
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-23 21:51:12 -0500

    Reaction model first draft complete

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,38 +1,33 @@
-const { Schema, model } = require('mongoose');
+const { Schema, Types } = require('mongoose');
+// const dateFormat = require('../utils/dateFormat')
 
-const courseSchema = new Schema(
+const reactionSchema = new Schema(
   {
-    courseName: {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
       type: String,
       required: true,
+      maxlength: 50,
     },
-    inPerson: {
-      type: Boolean,
-      default: true,
+    username: {
+      type: String,
+      required: true,
+      max_length: 50,
     },
-    startDate: {
+    createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
     },
-    endDate: {
-      type: Date,
-      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
-    },
-    students: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Student',
-      },
-    ],
   },
   {
     toJSON: {
-      virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
 
-const Course = model('course', courseSchema);
-
-module.exports = Course;
+module.exports = reactionSchema;
