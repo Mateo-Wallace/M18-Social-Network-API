abf047fcea13321d00e4e7187231a84601c8b98c
Mateo Wallace <mateo.t.wallace@gmail.com>
2022-11-24 23:15:35 -0500

    create thought functioning and connecting with user model

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,6 +27,25 @@ const thoughtController = {
   // create a new thought
   createThought(req, res) {
     // create on Thought
+    Thought.create(req.body)
+    .then((thought) => {
+      return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $addToSet: { thoughts: thought._id } },
+        { new: true }
+      );
+    })
+    .then((user) =>
+      !user
+        ? res.status(404).json({
+            message: 'Thought created, but found no user with that ID',
+          })
+        : res.json('Created the thought 🎉')
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
   },
 
   // update a thought
